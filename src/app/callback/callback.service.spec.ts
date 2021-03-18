2369d7a89d1c180d603d7e8e9687d5583c390be6
Édipo Bueno <edipo.bueno@gmail.com>
2021-03-18 06:56:55 -0300

    fogo no puteiro

---
+++ src/app/callback/callback.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { CallbackService } from './callback.service';
+
+describe('Service: Callback', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CallbackService]
+    });
+  });
+
+  it('should ...', inject([CallbackService], (service: CallbackService) => {
+    expect(service).toBeTruthy();
+  }));
+});
--- src/services/Okta/okta.service.ts
+++ src/services/Okta/okta.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AuthTransaction, OktaAuth, TokenResponse } from "@okta/okta-auth-js"
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Subscription } from 'rxjs';
+import { AccountService } from '../accounts/account.service';
+import { AuthServerService } from '../identity/auth-server.service';
 import { AuthServerModel } from '../identity/models/auth-server.model';
 import { LoadingService } from '../loading.service';
 import { SnackBarService } from '../snack-bar.service';
@@ -10,39 +13,49 @@ import { SnackBarService } from '../snack-bar.service';
 })
 export class OktaService {
 
+  private authServer!: AuthServerModel;
+  private authClient!: OktaAuth;
+  private authServersSubscription!: Subscription;
+
   constructor(
     private loadings: LoadingService,
     private snackBars: SnackBarService,
-    private oAuthService: OAuthService
+    private oAuthService: OAuthService,
+    private authServers: AuthServerService,
   ) {
-
+    this.authServersSubscription = this.authServers.authServerAsync().subscribe(
+      authServer => {
+        if (authServer) {
+          this.authServer = authServer;
+          this.authClient = new OktaAuth({
+            clientId: this.authServer.clientId,
+            authorizeUrl: this.authServer.authUrl,
+            tokenUrl: this.authServer.accessTokenUrl,
+            issuer: this.authServer.issuer,
+          });
+        }
+      }
+    );
   }
 
 
-  public auth(username: string, password: string, authServer: AuthServerModel, callback: (token: string) => void): void {
+  public auth(username: string, password: string): void {
     this.oAuthService.createAndSaveNonce().then(
       nonce => {
-        return this.signUp(username, password, authServer, nonce, callback);
+        return this.signUp(username, password, nonce);
       });
   }
 
-  private signUp(username: string, password: string, authServer: AuthServerModel, nonce: string, callback: (token: string) => void): Promise<void> {
+  private signUp(username: string, password: string, nonce: string): Promise<void> {
 
-    const authClient = new OktaAuth({
-      clientId: authServer.clientId,
-      authorizeUrl: authServer.authUrl,
-      tokenUrl: authServer.accessTokenUrl,
-      issuer: authServer.issuer,
-    });
-
-    return authClient.signInWithCredentials(
+    return this.authClient.signInWithCredentials(
       {
         username: username,
         password: password,
       }
     ).then(
       (response) => {
-        this.authorized(authClient, response, authServer, nonce, callback);
+        this.authorized(response, nonce);
       }
     ).catch(
       (error) => {
@@ -53,22 +66,19 @@ export class OktaService {
   }
 
 
-  private authorized(authClient: OktaAuth, authTransaction: AuthTransaction, authServer: AuthServerModel, nonce: string, callback: (token: string) => void): Promise<void> {
+  private authorized(authTransaction: AuthTransaction, nonce: string): Promise<void> {
     if (authTransaction.status === 'SUCCESS') {
-      return authClient.token.getWithoutPrompt(
+      this.loadings.setLoading('auth', false);
+      return this.authClient.token.getWithRedirect(
         {
-          clientId: authServer.clientId,
-          responseType: ['id_token', 'token', 'refresh_token'],
-          scopes: authServer.basicScopes,
+          clientId: this.authServer.clientId,
+          responseType: ['id_token', 'token'],
+          scopes: this.authServer.basicScopes,
           sessionToken: authTransaction.sessionToken,
           nonce: nonce,
           redirectUri: window.location.origin + '/callback'
         }
-      ).then(
-        (responseTokens) => {
-          this.validateToken(responseTokens, callback);
-        }
-      );
+      )
     }
     else {
       this.loadings.setLoading('auth', false);
@@ -79,26 +89,18 @@ export class OktaService {
     }
   }
 
-  private validateToken(response: TokenResponse, callback: (token: string) => void): void {
-    const idToken = response.tokens.idToken?.idToken;
-    const accessToken = response.tokens.accessToken?.accessToken;
-    const refreshToken = response.tokens.refreshToken?.refreshToken;
-    const keyValuePair = `id_token=${encodeURIComponent(idToken!)}&access_token=${encodeURIComponent(accessToken!)}&refresh_token=${encodeURIComponent(refreshToken!)}`;
-
-    this.redirecto(keyValuePair, callback)
+  public validateToken(): void {
+    this.authClient.token.parseFromUrl()
+      .then(response => {
+        this.token(response);
+      });
   }
 
-  private redirecto(keyValuePair: string, callback: (token: string) => void): void {
-    this.oAuthService.tryLogin(
-      {
-        customHashFragment: keyValuePair,
-        disableOAuth2StateCheck: true,
-      }
-    ).then(
-      () => {
-        this.loadings.setLoading('auth', false);
-        callback(`#${keyValuePair}`);
-      }
-    );
+  public isLoginRedirect = (): boolean => this.authClient.isLoginRedirect();
+
+  private token(response: TokenResponse): void {
+    const accessToken = response.tokens.accessToken?.accessToken;
+
+    location.href = `${sessionStorage.getItem('callbackUrl') || ''}?auth=${accessToken}`;
   }
 }
